a87db0662302a09af90e5c8603f6b5554ecf84c0
ROSE WINLET <124548530+rosewinlet@users.noreply.github.com>
2024-11-14 09:09:33 +0700

    Update 00_vpset.sh

--- noda/00_vpset.sh
+++ noda/00_vpset.sh
@@ -7,24 +7,23 @@ echo "Installing jq..."
 sudo apt update
 sudo apt install -y jq
 echo "jq installed successfully."
-apt install htop -y
-apt install screen -y
-apt install curl -y
+sudo apt install htop -y
+sudo apt install screen -y
+sudo apt install curl -y
 
-apt install net-tools
+sudo apt install net-tools unzip -y
 
 # ------------------------------------------------
 # Create a user:
-echo "You should enter the user password here!"
 sudo adduser media     
 usermod -a -G sudo media 
 
 # ------------------------------------------------
 # Add ssh key
 mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys
-chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
+sudo chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
 str="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/8NUhO5As50dXhqO+BimbtOIyyPniwHuRwp3xlH/meRJCeUAIjwbl4RHcEUqrFEvy0H1qPoow6I9G+7KJ0rfZkjCeEZ8+3thTnx4saFft6ho4k4/lWroz6yPPTfJFlCZleYthG6UAG5WpO0wjZm6yWuf2PqWYbF2eoqR3XbfkbDR7+TSRqoEvWwffyYMiEqGFRxJXNozgKjY9FSKXnE6qj7/gCCU0aDOjBU6AuOJx4ZdEvhNIY3qipx/9PwSlcs1ZWo/aLS5PU9ZY/5rjp7knLghTciqkPWS78M6NZtVchCqCKvGiKb0DKAQwGqwZDW0Hnz81/W00ibNPMlxm/p4tlwwG82XA9hras1Cr0wsp4hSqSiHqnn4OMFoZn8UMyOhyirXUxjh53/HGIRrn/KktahZ8Ztzp9GVEQQ9+wSGsN3qklfsnYrBmkSIYk7gBHwtbtm4T59bMkLnudtR0OWV06u5cOYNwkd2svGauqi4QrBF3fX/HMhqCDyUSmGZXCXs= "
-echo "$str" >> ~/.ssh/authorized_keys
+sudo echo "$str" >> ~/.ssh/authorized_keys
 
 
 # ------------ Change SSH port for server -------------
@@ -44,7 +43,7 @@ fi
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 # Update the SSH port in the sshd_config file
 #sed -i "s/^#Port .*/Port $new_port/; s/^Port .*/Port $new_port/" /etc/ssh/sshd_config
-sed -i \
+sudo sed -i \
     -e "s/^Port [0-9]\+/Port $new_port/; t found" \
     -e "s/^#Port [0-9]\+/Port $new_port/; t found" \
     -e "\$a\Port $new_port" \
@@ -52,7 +51,7 @@ sed -i \
     /etc/ssh/sshd_config
 
 # Restart the SSH service to apply changes
-service ssh restart
+sudo service ssh restart
 
 echo "SSH port changed to $new_port"
 # ---------------- Remove when not use ----------------
@@ -117,7 +116,6 @@ sudo systemctl start fwsys.service
 #sudo systemctl disable fwsys.service
 
 
-
 # ------------------------------------------------
 # Remote desktop relating code
 
@@ -129,7 +127,6 @@ sudo rm /etc/apt/sources.list.d/brave-browser-*.list
 sudo apt-get purge ubuntu-desktop -y 
 sudo apt-get purge ubuntu-desktop-minimal -y
 sudo apt-get autoremove -y 
-
 
 # Reinstall dekstop
 # Script to install ubuntu desktop & chrome
@@ -153,10 +150,11 @@ systemctl start slim
 sudo systemctl disable apt-daily.timer
 sudo systemctl disable apt-daily-upgrade.timer
 
-rm ~/common.sh
+#rm ~/common.sh
 
 # Install chrome remote desktop
 su - media
 # Link -> copy bash script
 
 history -c
+
